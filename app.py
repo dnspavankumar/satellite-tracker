440fd4ebc81c6014b877f2f3302dbb05866c1b2a
dnspavankumar <dnspavankumar722@gmail.com>
2025-03-14 12:43:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -354,4 +354,4 @@ if __name__ == '__main__':
 </html>''')
     
     print("Starting server. Access the app at http://localhost:5000")
-    app.run(debug=True, port=5000)
+    app.run(debug=True,host='0.0.0.0',port=5000)
